c6172a7678a24b01453f5c85ab12202bbe267eac
Andrés Enrique Lungoci <98490415+aelungoci@users.noreply.github.com>
2022-05-25 17:37:16 +0200

    v1.0.0

--- index.php
+++ index.php
@@ -15,24 +15,24 @@
 </body>
         
     <script>
-        //Recuperamos los datos de la API en un JSON
+        // Recuperamos los datos de la API en un JSON
         let datos = '';
         fetch('https://localhost/colegio/wp-json/wp/v2/posts')
             .then(result => result.json())
             .then((output) => {
-                //Asignamos los datos del JSON a una variable
+                // Asignamos los datos del JSON a una variable
                 datos = output; // Los datos del JSON están en formato array, por lo que ahora datos es un array
                 
-                //Introducimos en el formulario los datos a insertar en la BD
+                // Introducimos en el formulario los datos a insertar en la BD
                 document.getElementById("titulo").value = datos[0]['title']['rendered']; // Cogemos del array el primer elemento (último post), el título
                 document.getElementById("contenido").value = datos[0]['content']['rendered'];
 
         }).catch(err => console.error(err));
 
-        //Enviamos el formulario, esperando 1s
+        // Enviamos el formulario, esperando 1s
         function enviarFormulario() {
             document.getElementById("form").submit(); 
-        }
-        setTimeout(enviarFormulario, 1000); // Es necesario esperar 1s para que le de tiempo a insertar los datos
+        } // Es necesario esperar 1s para que le de tiempo a insertar y comprobar los datos
+        setTimeout(enviarFormulario, 1000); // setTimeout(FUNCTION, ms)
     </script>
 </html>
